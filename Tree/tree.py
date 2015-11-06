4c13a1952afc60882bed13493d69071df5d4e0c7
Steve <steven.helferich@gmail.com>
2015-11-05 20:59:26 -0800

    adding BFS to tree.py

--- Tree/tree.py
+++ Tree/tree.py
@@ -3,12 +3,19 @@ Implementation of a tree data structure. Not necessarily a binary search
 tree. 
 """
 import collections
+import copy
 
 class TreeNode():
 	def __init__(self, val):
 		self.val = val
 		self.left = None
 		self.right = None
+
+	def __str__(self):
+		return str(self.val)
+
+	def __repr__(self):
+		return str(self.val)
 
 def buildTree(vals):
 	q = collections.deque()
@@ -30,10 +37,31 @@ def buildTree(vals):
 		q.append(cur)
 	return root
 
-def main():
-	array = [1,2,3,None,4]
-	root = buildTree(array)
+def bfs(q, target):
+	if len(q) > 0:
+		current_list = q.popleft()
+		path = copy.copy(current_list[1])
+		current = current_list[0]
+		path.append(current)
+		if current.val == target:
+			return path
+		if current.left:
+			q.append([current.left,path])
+		if current.right:
+			q.append([current.right,path])
+		return bfs(q, target)
+	else:
+		return None
 
+def main():
+	array = [1,2,3,4,5,6,7,8]
+	root = buildTree(array)
+	q = collections.deque()
+	q.append([root,[]])
+	target = 8
+	print bfs(q, target)
 
 if __name__ == "__main__":
 	main()
+
+
